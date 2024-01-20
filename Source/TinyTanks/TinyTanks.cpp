d152514953e97d25da88e3d6f99f0fe652cac9f2
KristianBakov <kristian.bakov@yahoo.com>
2024-01-20 16:01:49 +0000

    Initial ue5 peoject commit

---
+++ Source/TinyTanks/TinyTanks.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TinyTanks.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TinyTanks, "TinyTanks" );
