7735884c51f616665a0641bfeedb1fbf8d614f8d
KristianBakov <kristian.bakov@yahoo.com>
2024-01-21 19:47:54 +0000

    Setup for enhanced input system and adding mapping context to tank with appropriate axis

--- enc_temp_folder/be71c79eb3c950b93f142f1a6bc96d2/Tank.cpp
+++ enc_temp_folder/be71c79eb3c950b93f142f1a6bc96d2/Tank.cpp
@@ -5,8 +5,6 @@
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
 #include "InputMappingContext.h"
-#include "EnhancedInputComponent.h"
-#include <EnhancedInputSubsystems.h>
 
 ATank::ATank()
 {
@@ -22,15 +20,6 @@ ATank::ATank()
 void ATank::BeginPlay()
 {
 	Super::BeginPlay();
-
-	//Setup input mapping context
-	if (auto PlayerController = Cast<APlayerController>(Controller))
-	{
-		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
-		{
-			Subsystem->AddMappingContext(InputMappingContext, 0);
-		}
-	}
 }
 
 void ATank::Tick(float DeltaTime)
@@ -41,14 +30,8 @@ void ATank::Tick(float DeltaTime)
 void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
-
-	EnhancedInputComponent->BindAction(InputMove, ETriggerEvent::Triggered, this, &ATank::Move);
 }
 
 void ATank::Move(const FInputActionValue& Value)
 {
-	const FVector2D MoveVector = Value.Get<FVector2D>();
-	UE_LOG(LogTemp, Warning, TEXT("MoveVector: %s"), *MoveVector.ToString());
 }
