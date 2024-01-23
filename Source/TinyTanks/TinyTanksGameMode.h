777b23e27fe7808fc4ab274608369c09a8111787
KristianBakov <kristian.bakov@yahoo.com>
2024-01-23 12:49:10 +0000

    Added gamemode class

---
+++ Source/TinyTanks/TinyTanksGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TinyTanksGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TINYTANKS_API ATinyTanksGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
